fda27522475d5fc87bb2795786f620d204a2c83e
Siddhant0Pandey <pandeysiddhant0922@gmail.com>
2025-02-23 16:28:58 +0545

    components setup

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,97 @@
+import { Link } from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <header className="bg-white shadow-md">
+      <div className="container mx-auto px-4 py-4">
+        <nav className="flex justify-between items-center">
+          {/* Logo */}
+          <Link to="/" className="flex items-center">
+            <img className="h-10" src="img/logo.png" alt="Desart" />
+          </Link>
+
+          {/* Navbar Toggler (Mobile) */}
+          <button
+            className="lg:hidden text-gray-700"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="text-2xl">&#9776;</span>
+          </button>
+
+          {/* Navigation Links */}
+          <div className="hidden lg:flex space-x-6">
+            <Link className="text-gray-700 hover:text-black" to="/">
+              Home
+            </Link>
+            <Link className="text-gray-700 hover:text-black" to="/about">
+              About
+            </Link>
+
+            {/* Dropdown for Services */}
+            <div className="relative">
+              <button className="text-gray-700 hover:text-black">
+                Services
+              </button>
+              <div className="dropdown-menu absolute hidden bg-white text-gray-700 shadow-lg rounded mt-2 w-48">
+                <Link className="block px-4 py-2" to="/services">
+                  Service
+                </Link>
+                <Link className="block px-4 py-2" to="/services-detail">
+                  Service Details
+                </Link>
+              </div>
+            </div>
+
+            {/* Dropdown for Portfolio */}
+            <div className="relative">
+              <button className="text-gray-700 hover:text-black">
+                Portfolio
+              </button>
+              <div className="dropdown-menu absolute hidden bg-white text-gray-700 shadow-lg rounded mt-2 w-48">
+                <Link className="block px-4 py-2" to="/portfolio">
+                  Portfolio
+                </Link>
+                <Link className="block px-4 py-2" to="/portfolio-details">
+                  Portfolio Details
+                </Link>
+              </div>
+            </div>
+
+            {/* Dropdown for Blog */}
+            <div className="relative">
+              <button className="text-gray-700 hover:text-black">Blog</button>
+              <div className="dropdown-menu absolute hidden bg-white text-gray-700 shadow-lg rounded mt-2 w-48">
+                <Link className="block px-4 py-2" to="/blog-single">
+                  Blog Single
+                </Link>
+              </div>
+            </div>
+
+            <Link className="text-gray-700 hover:text-black" to="/contact">
+              Contacts
+            </Link>
+          </div>
+
+          {/* Search Bar */}
+          <div className="lg:flex items-center hidden">
+            <input
+              className="form-input px-4 py-2 rounded-lg border-2 border-gray-300 mr-4"
+              type="search"
+              placeholder="Search"
+            />
+            <button className="btn bg-blue-500 text-white px-4 py-2 rounded-lg">
+              <i className="zmdi zmdi-search"></i> Search
+            </button>
+          </div>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default NavBar;
