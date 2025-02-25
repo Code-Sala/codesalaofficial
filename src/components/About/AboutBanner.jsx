9ace80c443b882a6043d2497cd6c5ebf73660750
sadipkhatri21 <sadipkhatri123@gmail.com>
2025-02-25 12:13:57 +0545

    new component added

---
+++ src/components/About/AboutBanner.jsx
@@ -0,0 +1,29 @@
+function AboutBanner() {
+  return (
+    <>
+      <div className="section-banner bg-gray-200 h-55 w-[1200px] ml-[135px] mt-20 flex justify-center items-center">
+  <div className="card-banner bg-white p-8 w-[1080px] gap-25  shadow-lg flex">
+    <div className="happy-clients text-center  w-[270px]">
+      <h3 className="text-4xl font-bold">125 +</h3>
+      <p className="text-gray-600 pt-2 text-lg -pl-45">Happy Clients</p>
+    </div>
+    <div className="happy-clients text-center  w-[270px]">
+      <h3 className="text-4xl font-bold">250 +</h3>
+      <p className="text-gray-600 pt-2 text-lg">Successful Projects</p>
+    </div>
+    <div className="happy-clients text-center  w-[270px]">
+      <h3 className="text-4xl font-bold">40 +</h3>
+      <p className="text-gray-600 pt-2 text-lg">Team Members</p>
+    </div>
+    <div className="happy-clients text-center  w-[270px]">
+      <h3 className="text-4xl font-bold">20 +</h3>
+      <p className="text-gray-600 pt-2 text-lg">Awards Winning</p>
+    </div>
+  </div>
+</div>
+
+    </>
+  )
+}
+
+export default AboutBanner
